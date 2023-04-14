90a67e6010ef556640626bbcb9a5f363d28602ba
Maksym <maksymstovolos2002@gmail.com>
2023-04-14 10:55:59 -0400

    Initial Commit

---
+++ Coding_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coding_Project
+//
+//  Created by Max Stovolos on 4/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
